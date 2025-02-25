993992276cc89ad3edba46b8cb84ac0fa29180e8
fimennn <firmanhidayat193330@gmail.com>
2025-02-25 12:13:15 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # selenium-ui-test-digitalskola
+https://drive.google.com/file/d/1WD0tUpmGpJ2rJ5znGNrqLk3WYEXDrZru/view?usp=sharing
--- test/saucedemo.test.js
+++ test/saucedemo.test.js
@@ -1,50 +1,32 @@
-const { Builder, By, until } = require("selenium-webdriver");
-const { expect } = require("chai");
+const { Builder, By, until } = require('selenium-webdriver');
+const assert = require('chai').assert;
 
-describe("SauceDemo UI Tests", function () {
-  let driver;
+describe('SauceDemo UI Test', function () {
+    let driver;
 
-  before(async function () {
-    this.timeout(10000);
-    driver = await new Builder().forBrowser("chrome").build();
-  });
+    before(async function () {
+        driver = await new Builder().forBrowser('chrome').build();
+        await driver.get('https://www.saucedemo.com/');
+    });
 
-  after(async function () {
-    await driver.quit();
-  });
+    after(async function () {
+        await driver.quit();
+    });
 
-  it("should login successfully and navigate to dashboard", async function () {
-    await driver.get("https://www.saucedemo.com/");
+    it('User success login', async function () {
+        await driver.findElement(By.id('user-name')).sendKeys('standard_user');
+        await driver.findElement(By.id('password')).sendKeys('secret_sauce');
+        await driver.findElement(By.id('login-button')).click();
 
-    // Login
-    await driver.findElement(By.id("user-name")).sendKeys("standard_user");
-    await driver.findElement(By.id("password")).sendKeys("secret_sauce");
-    await driver.findElement(By.id("login-button")).click();
+        const url = await driver.getCurrentUrl();
+        assert.include(url, 'inventory.html', 'Login failed or wrong URL');
+    });
 
-    // Validate dashboard
-    await driver.wait(until.urlContains("inventory.html"), 5000);
-    const url = await driver.getCurrentUrl();
-    expect(url).to.include("inventory.html");
-  });
+    it('Add item to cart and validate', async function () {
+        await driver.findElement(By.className('btn_inventory')).click();
+        await driver.wait(until.elementLocated(By.className('shopping_cart_badge')), 5000);
 
-  it("should add an item to the cart", async function () {
-    await driver.get("https://www.saucedemo.com/");
-    
-    // Login
-    await driver.findElement(By.id("user-name")).sendKeys("standard_user");
-    await driver.findElement(By.id("password")).sendKeys("secret_sauce");
-    await driver.findElement(By.id("login-button")).click();
-
-    // Add item to cart
-    const addToCartBtn = await driver.findElement(By.className("btn_inventory"));
-    await addToCartBtn.click();
-
-    // Validate cart badge
-    const cartBadge = await driver.wait(
-      until.elementLocated(By.className("shopping_cart_badge")),
-      5000
-    );
-    const badgeText = await cartBadge.getText();
-    expect(badgeText).to.equal("1");
-  });
+        const cartBadge = await driver.findElement(By.className('shopping_cart_badge')).getText();
+        assert.equal(cartBadge, '1', 'Item not added to cart');
+    });
 });
